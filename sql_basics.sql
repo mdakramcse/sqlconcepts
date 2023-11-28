776310bb8f6641395e867216d98ae34dda408e61
mdakramcse <152082743+mdakramcse@users.noreply.github.com>
2023-11-28 17:47:50 +0530

    Create sql_basics.sql

---
+++ sql_basics.sql
@@ -0,0 +1,15 @@
+--In SQL, to select each and every column in a table, you can use * instead of manually typing out all the column names:
+
+SELECT *
+ FROM table_name;
+
+--The SQL WHERE clause can be combined with the SELECT statement  to output only certain rows, based on if they meet certain conditions:
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+--The WHERE clause is often used with other logical operators like = (equals) and != (not equals) to filter down rows.=,!=,<,>,<=,>=
+
+
+
