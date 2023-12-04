8ed82fd2e0dcc2e215b10789fe1ebafedad6bde4
mdakramcse <152082743+mdakramcse@users.noreply.github.com>
2023-12-04 06:31:11 +0530

    Update sql_basics.sql

--- sql_basics.sql
+++ sql_basics.sql
@@ -12,6 +12,14 @@ WHERE condition;
 --The WHERE clause is often used with other logical operators like = (equals) and != (not equals) to filter down rows.=,!=,<,>,<=,>=
 -- Review
 
+--5 Ways To Filter Data With WHERE in SQL
+
+--how WHERE allows us to filter rows based on specified conditions
+--how AND and OR allow you to combine multiple filtering conditions
+--how BETWEEN allows you to filter on a range of values
+--how IN allows you to specify a list of values that you'd like to filter on
+--how LIKE allows you to match a value against a pattern
+
 
 
 
